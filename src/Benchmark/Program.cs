b72a7aa1127ee46b875734b125b4f58c8a9153ad
Yago Ferreira <yagoferreira21@gmail.com>
2025-04-19 22:16:56 -0300

    Removing unused test code

--- src/Benchmark/Program.cs
+++ src/Benchmark/Program.cs
@@ -7,10 +7,6 @@ public static class Program
     public static void Main(string[] args)
     {
         BenchmarkRunner.Run<Benchmarks>();
-        //var b = new Benchmarks();
-        //b.Setup();
-        //Console.WriteLine(b.MinDiator_IMediator_Send().GetAwaiter().GetResult());
-        //b.MinDiator_IPublisher_Publish().GetAwaiter().GetResult();
     }
 }
 
